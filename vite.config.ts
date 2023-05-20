7ad1f8d2e02c4c99d10061a3be85b38e9d2f9a81
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2023-05-20 22:09:02 +0530

    deployment commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import reactRefresh from '@vite/plugin-react-refresh';
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      // Allow serving files from one level up to handle client-side routing
-      allow: ['..'],
-    },
-  },
-});
+})
